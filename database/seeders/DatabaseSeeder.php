691c89dd7c3359603f2e882675eb753e816e4313
samsobao98 <samugranvia@gmail.com>
2025-02-21 11:06:09 +0100

    Version final 3.0

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +16,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            [
+                'name' => 'Administrador',
+                'email' => 'adminSamu@administrador.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('asmr1234.,'),
+                'remember_token' => Str::random(10),
+            ],
+            [
+                'name' => 'Administrador2',
+                'email' => 'adminMarcos@administrador.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('asmr1234.,'),
+                'remember_token' => Str::random(10),
+            ]
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        ]);
     }
 }
