e1e54052e8d6bafcb8b3e53a4ad4523e8b74730d
samsobao98 <samugranvia@gmail.com>
2025-02-21 10:38:46 +0100

    Version Final 2.0

--- resources/views/dato/index.blade.php
+++ resources/views/dato/index.blade.php
@@ -17,7 +17,7 @@
 
                             <div class="float-right">
                                 <a href="{{ route('datos.create') }}" class="btn btn-primary btn-sm float-right" data-placement="left">
-                                  {{ __('Create New') }}
+                                  {{ __('Crear nuevo') }}
                                 </a>
                             </div>
                         </div>
@@ -60,15 +60,15 @@
                                             <td>
                                                 <form action="{{ route('datos.destroy', $dato->id) }}" method="POST">
                                                     <a class="btn btn-sm btn-primary" href="{{ route('datos.show', $dato->id) }}">
-                                                        <i class="fa fa-fw fa-eye"></i> {{ __('Show') }}
+                                                        <i class="fa fa-fw fa-eye"></i> {{ __('Mostrar') }}
                                                     </a>
                                                     <a class="btn btn-sm btn-success" href="{{ route('datos.edit', $dato->id) }}">
-                                                        <i class="fa fa-fw fa-edit"></i> {{ __('Edit') }}
+                                                        <i class="fa fa-fw fa-edit"></i> {{ __('Editar') }}
                                                     </a>
                                                     @csrf
                                                     @method('DELETE')
                                                     <button type="submit" class="btn btn-danger btn-sm" onclick="event.preventDefault(); confirm('¿Estás seguro de eliminar este empleado?') ? this.closest('form').submit() : false;">
-                                                        <i class="fa fa-fw fa-trash"></i> {{ __('Delete') }}
+                                                        <i class="fa fa-fw fa-trash"></i> {{ __('Borrar') }}
                                                     </button>
                                                 </form>
                                             </td>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('¡Sesion iniciada!') }}
                 </div>
             </div>
         </div>
